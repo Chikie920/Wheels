fb9a772dab411d28dbea786d2b90b1af9de2ad0b
Chikie920 <livewild0920@gmail.com>
2020-10-14 19:27:33 +0800

    update

--- C/rookie/ballgame.c
+++ C/rookie/ballgame.c
@@ -4,18 +4,26 @@
 void updateWithInput(void);
 void updateWithoutInput(void);
 
-int ball_x=20, ball_y=50;
-int board_x=50, board_y=200;
-const int SPEED = 20;
+int ball_x=50, ball_y=10;
+int board_x=50, board_y=400;
+int SPEED = 40;
+int goals = 0;
 char ch;
+char pr[] = "Your goals is ";
 
 int main(void)
 {
     initgraph(640,480,NULL);
     setbkcolor(WHITE);
+    delay_fps(100);
+    setcolor(BLACK);
+    circle(ball_x,ball_y,10);
+    line(board_x,board_y,board_x+80,board_y);
     while(1){
         updateWithoutInput();
         updateWithInput();
+        pr[14]=5;
+        xyprintf(10,430,"Your goal is %d",goals);
     }
 
     return 0;
@@ -23,22 +31,36 @@ int main(void)
 
 void updateWithInput(void)
 {
-    while(ch=getch()){
-    switch(ch){
-        case 'a': board_x-=20;
-            break;
-            case 'd': board_x+=20;
-            break;
+    setcolor(WHITE);
+    line(board_x,board_y,board_x+80,board_y);
+    if(kbhit()){
+        ch = getch();
+        switch(ch){
+            case 'a': board_x-=40;
+                break;
+            case 'd': board_x+=40;
+                break;
             default :
-            break;
-        }
-    outtextxy(board_x,board_y,"__________");}
+                break;
+            }
+    }
+    Sleep(100);
+    setcolor(BLACK);
+    line(board_x,board_y,board_x+80,board_y);
+    Sleep(100);
 }
 
 void updateWithoutInput(void)
 {
-    while(ball_y<=480){
-        ball_y+=SPEED;
-        circle(ball_x,ball_y,10);
+    setcolor(WHITE);
+    circle(ball_x,ball_y,10);
+    ball_y+=SPEED;
+    if(ball_y>=board_y-33&&ball_x>=board_x&&ball_x<board_x+80){
+        SPEED=-SPEED;
+        goals++;
     }
+    Sleep(100);
+    setcolor(BLACK);
+    circle(ball_x,ball_y,10);
+    Sleep(100);
 }
